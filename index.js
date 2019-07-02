897663a14e5e591f7d1b7c9485906ff0b6a56f19
Aude Faucheux <aude53@gmail.com>
2019-07-02 20:49:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value
   return object
 }
+
+function deleteFromObjectByKey(object, key) {
+  object2 = Object.assign({}, object)
+  delete object2.key
+  return object2
+}
